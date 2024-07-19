bf6daefdd6addad0d5b11d27f5e1d7a7500ffc58
sidneckel <sidn.ick2015@gmail.com>
2024-07-19 10:19:35 -0300

    tarefa 03

--- real-world-vue/src/main.js
+++ real-world-vue/src/main.js
@@ -1,5 +1,4 @@
-import './assets/main.css'
-
+// import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
@@ -12,3 +11,4 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
