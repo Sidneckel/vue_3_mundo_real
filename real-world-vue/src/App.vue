08b0c68469a9d3241f082c422b46c13128a17909
sidneckel <sidn.ick2015@gmail.com>
2024-08-01 11:26:43 -0300

    atualizando para tarefa 4

--- real-world-vue/src/App.vue
+++ real-world-vue/src/App.vue
@@ -1,14 +1,9 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-// import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div id="layout">
     <header>
       <div class="wrapper">
         <nav>
-          <RouterLink to="/">Home</RouterLink> |
+          <RouterLink to="/">Events</RouterLink> |
           <RouterLink to="/about">About</RouterLink>
         </nav>
       </div>
